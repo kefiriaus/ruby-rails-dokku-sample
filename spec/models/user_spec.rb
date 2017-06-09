4414e698f76e989326f611de52696fedfdad2e3a
Sergey Golovkin <kefiriaus@gmail.com>
2017-06-09 14:32:14 +0800

    Add rspec config and tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "with social auth" do
+    it "can auth with facebook" do
+    end
+    it "can auth with vk" do
+    end
+    it "can auth with ok" do
+    end
+    it "can auth with instagram" do
+    end
+  end
+end
